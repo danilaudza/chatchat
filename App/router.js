fd8ec5c7a43e8326f551ca44fc75f0014b98455b
danilaudza <danilaudza@gmail.com>
2021-06-05 12:48:26 +0700

    add splash

--- App/router.js
+++ App/router.js
@@ -1,9 +1,26 @@
+import React, {Component} from 'react';
+import { View, StatusBar } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack'
+import { createStackNavigator, TransitionPresets } from 'react-navigation-stack'
 import SignUp from '../App/Screens/SignUpScreen';
 import Login from '../App/Screens/LoginScreen';
 import Dashboard from '../App/Screens/DashboardScreen';
 import Chat from '../App/Screens/ChatScreen';
+import * as Animatable from 'react-native-animatable';
+
+class splash extends Component{
+    render(){
+        setTimeout(() => {
+            this.props.navigation.navigate('Auth');
+        }, 3000)
+        return(
+            <View style={{backgroundColor: '#6B48DE', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <StatusBar hidden/>
+            <Animatable.Image source={require('../assets/obrolwhite.png')} animation='zoomIn' easing='ease-in-out-expo' style={{alignSelf: 'center',resizeMode: 'center', width: '60%'}}/>
+            </View>
+        )
+    }
+}
 
 const AuthStack = createStackNavigator({
     Login: Login,
@@ -16,15 +33,17 @@ const DashboardStack = createStackNavigator({
     Dashboard: Dashboard,
     Chat:Chat
 }, {
-    initialRouteName: 'Dashboard', headerMode: 'none'
+    initialRouteName: 'Dashboard', headerMode: 'none', defaultNavigationOptions: {
+        ...TransitionPresets.SlideFromRightIOS
+    }
 });
 
 const App = createSwitchNavigator({
+    splash: splash,
     Auth: AuthStack,
     Dashboard: DashboardStack
 },
-    { initialRouteName: 'Auth' }
+    { initialRouteName: 'splash' }
 );
-
 
 export default createAppContainer(App);
